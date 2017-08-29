20e29bb32ef2722621223cbb70521d2fe4f156e5
Sarah Cooke <marshcooke1@gmail.com>
2017-08-29 18:36:04 -0500

    Return The Last Array - not returning w/o failing edabit but passing in my console.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('edibit-js');
+// beyond here is the js I worked on in Edibit.
+var arr = ["cat", "dog", "duck"];
+function getLastItem(arr) {
+    return (arr[arr.length - 1]);
+ }
+ console.log(getLastItem(arr));
