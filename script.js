2ea688eb98a2aa57712985d07a80366992ff03f5
Sarah Cooke <marshcooke1@gmail.com>
2017-08-30 08:52:31 -0500

    slight changes in peer code review

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('edibit-js');
 // beyond here is the js I worked on in Edibit.
-var arr = ["cat", "dog", "duck"];
+var arr = ['cat', 'dog', 'duck'];
 function getLastItem(arr) {
     return (arr[arr.length - 1]);
  }
